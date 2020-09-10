6776ce51136c0fcad478481132e54f3f29157359
thanhpt0105 <thanhpt0105@gmail.com>
2020-09-10 19:24:27 +0800

    Initial commit

---
+++ src/main/java/com/thanhpt0105/mcbrewery/McBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.thanhpt0105.mcbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhpt0105/mcbrewery/McBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhpt0105.mcbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
